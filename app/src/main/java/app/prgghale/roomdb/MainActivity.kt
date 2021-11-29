32675c06b767423fd3552d828af3748867a167ef
ghaleprachan <ghale.prachan@gmail.com>
2021-11-29 12:38:42 +0545

    first commit

---
+++ app/src/main/java/app/prgghale/roomdb/MainActivity.kt
@@ -0,0 +1,51 @@
+package app.prgghale.roomdb
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import app.prgghale.roomdb.ui.theme.RoomDBTheme
+
+class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            RoomDBTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Scaffold(
+                        bottomBar = { BottomBar() }
+                    ) {
+                        LazyColumn(contentPadding = it, content = {
+                            itemsIndexed((1..60).toList()) { index, item ->
+                                Greeting(name = "Test $index")
+                            }
+                        })
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    RoomDBTheme {
+        Greeting("Android")
+    }
+}
---
+++ app/src/main/java/app/prgghale/roomdb/TypeAlias.kt
@@ -0,0 +1,7 @@
+package app.prgghale.roomdb
+
+import androidx.compose.material.icons.Icons
+
+val iconFilled = Icons.Filled
+val iconOutlined = Icons.Outlined
+val iconDefault = Icons.Default
