d571375904eb16771b2bfb6b6fc3fd25f2e06e2c
Neo <c.min.tseng@gmail.com>
2025-09-05 16:09:06 +0800

    upgrade coil version

--- app/src/main/java/app/prgghale/roomdb/composables/Images.kt
+++ app/src/main/java/app/prgghale/roomdb/composables/Images.kt
@@ -1,13 +1,18 @@
 package app.prgghale.roomdb.composables
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
-import coil.compose.rememberImagePainter
-import coil.transform.CircleCropTransformation
-
+import coil3.compose.AsyncImage
+import coil3.request.ImageRequest
+import coil3.request.crossfade
+import coil3.request.transformations
+import coil3.transform.CircleCropTransformation
 @Composable
 fun UserProfileImg(
     modifier: Modifier = Modifier,
@@ -15,14 +20,18 @@ fun UserProfileImg(
     lastName: String?,
     contentDescription: String? = null,
 ) {
-    Image(
-        painter = rememberImagePainter(
-            data = "https://ui-avatars.com/api/?name=${firstName}+${lastName}",
-            builder = {
-                crossfade(true)
-                transformations(CircleCropTransformation())
-            }),
+    val imageUrl = "https://ui-avatars.com/api/?name=${firstName?.trim() ?: ""}+${lastName?.trim() ?: ""}&background=random"
+
+    AsyncImage(
+        model = ImageRequest.Builder(LocalContext.current)
+            .data(imageUrl)
+            .crossfade(true) // Enable crossfade
+            .transformations(CircleCropTransformation())
+            .build(),
         contentDescription = contentDescription,
-        modifier = modifier.size(50.dp)
+        modifier = modifier
+            .size(50.dp)
+            .clip(CircleShape),
+        contentScale = ContentScale.Crop
     )
 }
